2d3ac5379f109f338446ac8e4b233e9290ba84f8
nengkya <nengkya@yahoo.com>
2025-03-01 01:27:54 +0700

    a

--- CreferenceCounting/referenceCounting.c
+++ CreferenceCounting/referenceCounting.c
@@ -15,26 +15,31 @@ struct refcount {
 static void * rc_malloc(size_t size, deallocator free) { /*void * require return value or it will raise a warning*/
 
 	/*
-	The addition of + size in the malloc call is necessary because the memory allocation needs to include space for both the metadata (struct refcount)
+	The addition of + size in the malloc call is necessary
+	because the memory allocation needs to include space for both the metadata (struct refcount)
 	and the actual user data requested by the caller.
 	*/
 	struct refcount * p = malloc(sizeof(struct refcount) + size);
 
-	if (p) /*suppresse warning about possibility dereferencing nullptr*/
-	* p = (struct refcount){.count = 1, .free = free};
+	/*suppresse warning about possibility dereferencing nullptr*/
+	if (p) * p = (struct refcount){.count = 1, .free = free};
 
 	/*
-	p + 1 increments p by 1 unit of the size of struct refcount, effectively moving the pointer past the metadata portion of the memory.
+	p + 1 increments p by 1 unit of the size of struct refcount,
+	effectively moving the pointer past the metadata portion of the memory.
 	
 	1. Separate Metadata from User Data:
 	   a. The allocated memory block includes both :
 		  1. Metadata (struct refcount): Used internally by the memory management system.
 		  2. User data: The memory requested by the caller.
-	   b. By returning (p + 1), the user only sees the memory intended for their use, and they remain unaware of the metadata.
+	   b. By returning (p + 1), the user only sees the memory intended for their use,
+	   	  and they remain unaware of the metadata.
 	
 	2. Encapsulation:
-	   a. This design encapsulates the reference-counting mechanism, preventing the caller from accidentally modifying the metadata.
-	   b. The caller interacts only with the memory they requested, without worrying about how the reference count is managed
+	   a. This design encapsulates the reference-counting mechanism,
+	   	  preventing the caller from accidentally modifying the metadata.
+	   b. The caller interacts only with the memory they requested,
+	   	  without worrying about how the reference count is managed
 	*/
 	return p + 1;
 
@@ -54,7 +59,6 @@ static void * incref(void * p) {
 
 	if (!p) return p;
 
-
 }
 
 
@@ -68,7 +72,8 @@ int main() {
 	int * p = rc_malloc(sizeof * p, print_free);
 
 	/*
-	Assigning 42 ensures there’s a visible, meaningful value stored in the memory that can be observed when it is eventually freed.
+	Assigning 42 ensures there is a visible,
+	meaningful value stored in the memory that can be observed when it is eventually freed.
 	Ensures that the allocated memory contains a meaningful value (42) that can be printed during deallocation,
 	making the behavior of the program more observable and meaningful in this example.
 	*/
---
+++ referenceCounting.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+
+void * rc_malloc() {
+
+
+
+
+
+
+}
+
+
+int main() {
+
+	/*
+	sizeof(datatype);
+	sizeof expression;
+ 	printf("%ld", sizeof 1);
+	*/
+
+	int * p = rc_malloc();
+
+
+
+
+
+
+}
