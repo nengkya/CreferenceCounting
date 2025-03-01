8563151f45c23cd67b383ec7ece69ca79b44206d
nengkya <nengkya@yahoo.com>
2025-03-01 20:21:25 +0700

    struct refcount * p = (struct refcount *)malloc(sizeof(struct refcount) + size);

--- referenceCounting.c
+++ referenceCounting.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h" /*malloc();*/
 
 /* deallocator, is a pointer to function that take void pointer as a parameter, returning void */
 typedef void(* deallocator)(void *);
@@ -8,7 +9,12 @@ struct refcount {int count; deallocator free;};
 
 void * rc_malloc(size_t size, deallocator free) {
 
-
+	/*
+	the addition of + size in the malloc call is necessary
+	because the memory allocation needs to include space both metadata (refcount)
+	and the actual user data requested by the caller
+	*/
+	struct refcount * p = (struct refcount *)malloc(sizeof(struct refcount) + size);
 
 
 
@@ -33,7 +39,7 @@ int main() {
  	printf("%ld", sizeof 1);
 	*/
 
-	int * p = rc_malloc(size of * p, print_free);
+	int * p = rc_malloc(sizeof * p, print_free);
 
 
 
